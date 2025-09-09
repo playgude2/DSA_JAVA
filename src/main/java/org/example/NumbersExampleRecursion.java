75c18e09954aef5a091ebf03bdfd1e1af142d615
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-09 19:53:27 +0530

    numbersExampleRecursion

--- src/main/java/org/example/NumbersExampleRecursion.java
+++ src/main/java/org/example/NumbersExampleRecursion.java
@@ -1,2 +1,15 @@
-package org.example;public class NumbersExampleRecursion {
+package org.example;
+
+public class NumbersExampleRecursion {
+    public static void main(String[] args) {
+        printNumber(1);
+    }
+
+    static void printNumber(int n){
+        if(n == 6){
+            return;
+        }
+        System.out.println(n);
+        printNumber(n+1);
+    }
 }
