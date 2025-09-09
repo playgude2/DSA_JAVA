2278dbece1b38f182b419c55872fac1934d693d7
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-09 19:29:34 +0530

    Initial commit

---
+++ src/main/java/org/example/Message.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Message {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+    message();
+    }
+
+
+    static void message(){
+        System.out.println("Message1");
+    }
+}
