48aecb396e4743636de0aed3e1f28a2c616c478b
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-11 20:14:36 +0530

    SumofNumberRecursion

--- src/main/java/easy/FactorialNumber.java
+++ src/main/java/easy/FactorialNumber.java
@@ -2,11 +2,11 @@ package easy;
 
 public class FactorialNumber {
     public static void main(String[] args) {
-        System.out.println(fact(5));
+        System.out.println(fact(6));
     }
     static int fact(int n){
-        if(n == 1){
-            return n;
+        if(n <= 1){
+            return 1;
         }
         return n*fact(n-1);
     }
--- src/main/java/easy/SumOfNumbersRecursion.java
+++ src/main/java/easy/SumOfNumbersRecursion.java
@@ -1,2 +1,14 @@
-package easy;public class SumOfNumbers {
+package easy;
+
+public class SumOfNumbersRecursion {
+    public static void main(String[] args) {
+        System.out.println(sumNumbers(1340212));
+    }
+
+    static int sumNumbers(int n){
+        if(n == 0){
+            return 0;
+        }
+        return sumNumbers(n/10) + n%10;
+    }
 }
