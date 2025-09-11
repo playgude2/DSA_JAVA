5ded74cf954454879efedd4b9182be4ee4aebf97
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-11 20:05:17 +0530

    FactorialNumberRecursion

---
+++ src/main/java/easy/FactorialNumber.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+    static int fact(int n){
+        if(n == 1){
+            return n;
+        }
+        return n*fact(n-1);
+    }
+}
--- src/main/java/easy/Numbers1toNRecursion.java
+++ src/main/java/easy/Numbers1toNRecursion.java
@@ -1,2 +1,15 @@
-package easy;public class Numbers1toNRecursion {
+package easy;
+
+public class Numbers1toNRecursion {
+    public static void main(String[] args) {
+        Num1toN(5);
+    }
+
+    static void Num1toN(int n){
+        if(n==0){
+            return;
+        }
+        Num1toN(n-1);
+        System.out.println(n);
+    }
 }
