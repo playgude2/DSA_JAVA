1527dde3188ef28e29219cadfdacd9743f418259
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-12 20:03:34 +0530

    ReverseofNumberRecursio updaten

--- src/main/java/easy/ReverseNumberRecursion.java
+++ src/main/java/easy/ReverseNumberRecursion.java
@@ -1,13 +1,26 @@
 package easy;
 
 public class ReverseNumberRecursion {
-    public static void main(String[] args) {
-        System.out.println(reverseNum(43521, 0));
-    }
-    static int reverseNum(int n, int rev){
-        if(n == 0){
-            return rev;
+    static int sum = 0;
+    static void reverseNum2(int n){
+        if(n==0){
+            return;
         }
-        return reverseNum(n/10, rev*10 + n%10);
+        int rem = n%10;
+        sum = sum * 10 + rem;
+        reverseNum2(n/10);
     }
+    public static void main(String[] args) {
+//        System.out.println(reverseNum1(43521, 0));
+        reverseNum2(2314);
+        System.out.println(sum);
+    }
+//    static int reverseNum1(int n, int rev){
+//        if(n == 0){
+//            return rev;
+//        }
+//        return reverseNum1(n/10, rev*10 + n%10);
+//    }
+
+
 }
