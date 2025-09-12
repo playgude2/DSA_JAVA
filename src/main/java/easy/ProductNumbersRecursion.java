61792a3fe114d47120f52ba54117740c170e08b7
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-12 19:27:12 +0530

    ProductofNumberRecursion

---
+++ src/main/java/easy/ProductNumbersRecursion.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class ProductNumbersRecursion {
+    public static void main(String[] args) {
+        System.out.println(productNum(124320));
+    }
+
+    static int productNum(int n){
+        if(n <= 1){
+            return 1;
+        }
+        return productNum(n/10) * (n%10);
+    }
+}
