018f2bc43172e7dabc3b35eb8319e8b5b15633ff
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-18 17:36:02 +0530

    reverse number

---
+++ src/main/java/easy/ReverseNumber.java
@@ -0,0 +1,12 @@
+package easy;
+public class ReverseNumber {
+    static int reverse(int n, int rev) {
+        if (n == 0) return rev;
+        return reverse(n / 10, rev * 10 + n % 10);
+    }
+
+    public static void main(String[] args) {
+        int num = 3421;
+        System.out.println("Reversed: " + reverse(num, 0)); // Output: 1243
+    }
+}
