e2c822f477ed728086007d1bac988aee587872bc
Pranav Laygude <123pranavlaygude@gmail.com>
2025-09-22 18:36:55 +0530

    nto1 number

---
+++ src/main/java/easy/Nto1.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        nto1(5);
+    }
+
+    static void nto1(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        nto1(n-1);
+    }
+}
