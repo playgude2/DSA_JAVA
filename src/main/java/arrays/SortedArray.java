fa80a41f5129cb070738670ef6b2a3656f5b6c09
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-01 19:31:38 +0530

    sorted  arrays or not

--- src/main/java/arrays/SortedArray.java
+++ src/main/java/arrays/SortedArray.java
@@ -1,4 +1,19 @@
 package arrays;
 
 public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 4, 2, 6, 7, 8};
+        int[] arr2 = {1, 4, 6, 7, 8};
+        System.out.println(sorted(arr1, 0));
+        System.out.println(sorted(arr2, 0));
+
+    }
+
+    static boolean sorted(int[] arr, int index){
+        if(index == arr.length -1){
+            return true;
+        }
+        return arr[index]< arr[index+1] && sorted(arr, index+1);
+
+    }
 }
