841730eaf1bd21331f236bbe1b0d847d58c8db94
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-01 19:45:20 +0530

    linear search with index prgm

--- src/main/java/arrays/LinearSearch.java
+++ src/main/java/arrays/LinearSearch.java
@@ -3,8 +3,10 @@ package arrays;
 public class LinearSearch {
     public static void main(String[] args) {
         int[] arr1 = {1, 34, 12, 76, 9};
-        int target = 1;
+        int target = 122;
         System.out.println(linearSearch(arr1, target, 0));
+        System.out.println(linearSearchIndex(arr1, target, 0));
+
     }
 
     static boolean linearSearch(int[] arr, int target, int index){
@@ -15,5 +17,17 @@ public class LinearSearch {
             return true;
         }
         return linearSearch(arr, target, index+1);
+    }
+
+    static int linearSearchIndex(int[] arr, int target, int index){
+        if(index == arr.length){
+            return -1;
+        }
+        if(arr[index] == target){
+            return index;
+        }
+        else{
+            return linearSearchIndex(arr, target, index+1);
+        }
     }
 }
