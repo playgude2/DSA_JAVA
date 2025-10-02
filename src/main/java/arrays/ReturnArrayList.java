c94f5b357e334c6ce813a97fcbcc5e47cb8b796a
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-02 21:46:38 +0530

    return an array list prgm

--- src/main/java/arrays/ReturnArrayList.java
+++ src/main/java/arrays/ReturnArrayList.java
@@ -1,4 +1,21 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class ReturnArrayList {
+    public static void main(String[] args) {
+            int [] arr = {1, 3, 4, 87, 65, 4, 14, 11, 10};
+            int target = 99;
+        System.out.println(findAllIndex(arr, target, 0, new ArrayList<>()));
+    }
+
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index == arr.length){
+            return list;
+        }
+        if(target== arr[index]){
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index+1, list);
+    }
 }
