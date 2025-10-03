259e44987063e343b52173fb7911d5d5933f12ad
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-03 19:19:25 +0530

    update:roteted Binary search prgm

--- src/main/java/arrays/RotetedBinarySearch.java
+++ src/main/java/arrays/RotetedBinarySearch.java
@@ -1,4 +1,34 @@
 package arrays;
 
 public class RotetedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3};
+        int target = 3;
+        System.out.println(rbs(arr, target, 0, arr.length -1));
+    }
+
+    static int rbs(int[] arr, int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+
+        int m = s + (e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+
+        if(arr[s] <= arr[m]){
+            if(target >= arr[s] && target <= arr[m]){
+                return rbs(arr, target, s, m-1);
+            }
+            else{
+                return rbs(arr, target, m+1, e);
+            }
+        }
+
+        if(target >= arr[m] && target <= arr[e]){
+            return rbs(arr, target, m+1, e);
+        }
+        return rbs(arr, target, s, m-1);
+    }
 }
