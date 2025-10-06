bc2b23a064c068c496d88721fad9436013a6876e
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-06 19:03:35 +0530

    New: added new triangle pattern prgm

--- src/main/java/patterns/Triangle.java
+++ src/main/java/patterns/Triangle.java
@@ -1,4 +1,21 @@
 package patterns;
 
 public class Triangle {
+    public static void main(String[] args) {
+        triangle(4, 0);
+    }
+
+    static void triangle(int r, int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            System.out.print("x");
+            triangle(r, c+1);
+        }
+        else{
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
 }
