d5fd7ba0fcce767276bd1260a6402323f5425604
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-07 22:07:52 +0530

    update : selection sort using recursion prgm

--- src/main/java/patterns/SelectionSort.java
+++ src/main/java/patterns/SelectionSort.java
@@ -1,4 +1,29 @@
 package patterns;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1,4,8,2,9};
+        selectionSort(arr,arr.length-1,0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr, int r, int c, int maxIndex){
+        if(r==0){
+            return;
+        }
+
+        if (c < r) {
+            if (arr[c]>arr[maxIndex]){
+                int temp = arr[c];
+                arr[c] = arr[maxIndex];
+                arr[maxIndex] = temp;
+            }
+            selectionSort(arr, r, c + 1, maxIndex);
+        }
+        else{
+            selectionSort(arr, r-1, 0, maxIndex);
+        }
+    }
 }
