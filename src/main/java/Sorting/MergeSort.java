0ae50125bc56a935ae5f5e9b283a3732fb0cf1ba
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-08 19:25:13 +0530

    new : added main logic of merge sorting prgm

--- src/main/java/Sorting/MergeSort.java
+++ src/main/java/Sorting/MergeSort.java
@@ -13,7 +13,39 @@ public class MergeSort {
         }
         int mid = arr.length / 2;
         int[] left = merge(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = merge(Arrays)
+        int[] right = merge(Arrays.copyOfRange(arr, mid, arr.length));
+        return mergeCombine(left, right);
+    }
 
+    static int[] mergeCombine(int[] left, int[] right){
+        int[] mix = new int[left.length + right.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                mix[k]=left[i];
+                i++;
+            }
+            else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i<left.length){
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+        while (j<right.length){
+            mix[k]=right[j];
+            j++;
+            k++;
+        }
+        return mix;
     }
 }
