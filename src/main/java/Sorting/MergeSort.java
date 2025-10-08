218514405339effc7f9551ae5277ae85c99cadca
Pranav Laygude <123pranavlaygude@gmail.com>
2025-10-08 19:27:47 +0530

    update : main logic of merge sorting prgm

--- src/main/java/Sorting/MergeSort.java
+++ src/main/java/Sorting/MergeSort.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {5,4,3,2,1};
+
+        arr = merge(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static int[] merge(int[] arr){
